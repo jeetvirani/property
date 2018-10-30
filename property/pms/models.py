7b101c09f7f97cb2ebbbb0b2538aa1ee197c29fe
jeet <jeet@vumonic.com>
2018-10-30 23:41:06 +0530

    models

--- property/pms/models.py
+++ property/pms/models.py
@@ -46,5 +46,71 @@ class User(AbstractUser, BaseModel):
     company_profile = models.CharField(max_length=50)
     user_type = models.ManyToManyField(UserType, related_name='user_type')
 
+class NearBy(models.Model):
+    nearby_Id = models.AutoField(primary_key=True)
+    landmark = models.CharField(max_length=50)
+    hospital = models.CharField(max_length=50)
+    railway = models.CharField(max_length=50)
+    school = models.CharField(max_length=50)
+    class Meta:
+        db_table = 'near_by'
+
+class Location(models.Model):
+    location_id = models.AutoField(primary_key=True)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    pincode = models.CharField(max_length=50)
+    society = models.CharField(max_length=50)
+    latitude = models.CharField(max_length=50)
+    longiture = models.CharField(max_length=50)
+    class Meta:
+        db_table = 'location'  
+
+class Amenities(models.Model):
+    amenity_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    class Meta:
+        db_table = 'amenities'  
+
+class Document(models.Model):
+    doc_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    image_url = models.CharField(max_length=50)
+    class Meta:
+        db_table = 'document'    
+
+class TransactionType(models.Model):
+    trans_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    class Meta:
+        db_table = 'transaction_type'         
+
+class Property(AbstractProperty,BaseModel):
+    nearby = models.ManyToManyField(NearBy, related_name='near_by')
+    location = models.ManyToManyField(Location, related_name='location')
+    amenity = models.ManyToManyField(Amenities, related_name='amenities')
+    document = models.ManyToManyField(Document, related_name='document')
+    transaction_type = models.ManyToManyField(TransactionType, related_name='transaction_type')
+    property_type = models.ManyToManyField(PropertyType, related_name='property_type')
+    budget = models.CharField(max_length=50)
+    duration = models.CharField(max_length=50)
+    bedrooms = models.CharField(max_length=50)
+    description = models.CharField(max_length=50)
+    bathrooms = models.CharField(max_length=50)
+    company_name = models.CharField(max_length=50)
+    furnished = models.CharField(max_length=50)
+    contact_person = models.CharField(max_length=50)
+    no_of_floors = models.CharField(max_length=50)
+    no_of_carparking = models.CharField(max_length=50)
+    posted_by = models.CharField(max_length=50)
+    no_of_floors = models.CharField(max_length=50)
+    property_type = models.CharField(max_length=50)
+    builtup_area = models.CharField(max_length=50)
+    is_buyable = models.BooleanField(default=False)
+    user_type = models.ManyToManyField(UserType, related_name='user_type')
+
+
+
+
 
 
